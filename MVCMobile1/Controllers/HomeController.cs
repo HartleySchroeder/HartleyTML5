27deade3c74250f262366132f4335024a1ae11d2
hartlesss <hartley.c.schroeder@gmail.com>
2012-12-03 10:25:03 -0600

    First Update

--- MVCMobile1/Controllers/HomeController.cs
+++ MVCMobile1/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace MVCMobile1.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Welcome to ASP.NET MVC!";
+            ViewBag.Message = "Welcome to ASP.NET MVC!  This is cool!";
 
             return View();
         }
